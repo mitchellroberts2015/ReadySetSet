0b2f633ec4fe731c80315bbc496507c712a64b67
Michael Stevens <mls0.5mv2@gmail.com>
2019-04-13 22:40:24 -0400

    added basic segmentation

--- ReadySetSet/app/src/main/java/com/example/readysetset/MainActivity.java
+++ ReadySetSet/app/src/main/java/com/example/readysetset/MainActivity.java
@@ -18,26 +18,17 @@ import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-import org.opencv.core.Scalar;
-import org.opencv.core.Size;
 
 public class MainActivity extends Activity implements OnTouchListener, CvCameraViewListener2 {
     private static final String TAG = "MainActivity";
 
-    private boolean mIsColorSelected = false;
-    private Mat mRgba;
-    private Scalar mBlobColorRgba;
-    private Scalar mBlobColorHsv;
-    //    private ColorBlobDetector    mDetector;
-    private Mat mSpectrum;
-    private Size SPECTRUM_SIZE;
-    private Scalar CONTOUR_COLOR;
-
     private CameraBridgeViewBase mOpenCvCameraView;
 
     private Segmenter segmenter;
+
+    // which mat to display
+    private int display_i = 0;
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -47,7 +38,7 @@ public class MainActivity extends Activity implements OnTouchListener, CvCameraV
                     Log.i(TAG, "OpenCV loaded successfully");
                     mOpenCvCameraView.enableView();
                     mOpenCvCameraView.setOnTouchListener(MainActivity.this);
-                    mOpenCvCameraView.setMaxFrameSize(640,480);
+                    mOpenCvCameraView.setMaxFrameSize(640*3,480*3);
                 }
                 break;
                 default: {
@@ -105,89 +96,20 @@ public class MainActivity extends Activity implements OnTouchListener, CvCameraV
     }
 
     public void onCameraViewStarted(int width, int height) {
-        Log.e("SET", "width: "  + width + ", height: " + height);
-        mRgba = new Mat(height, width, CvType.CV_8UC4);
-//        mDetector = new ColorBlobDetector();
-        mSpectrum = new Mat();
-        mBlobColorRgba = new Scalar(255);
-        mBlobColorHsv = new Scalar(255);
-        SPECTRUM_SIZE = new Size(200, 64);
-        CONTOUR_COLOR = new Scalar(255, 0, 0, 255);
-        segmenter = new Segmenter(width, height, 1/2);
+        segmenter = new Segmenter(width, height, 1/3);
     }
 
     public void onCameraViewStopped() {
-//        mRgba.release();
+        segmenter.release();
     }
 
     public boolean onTouch(View v, MotionEvent event) {
-        // we dont care about touching
-//        int cols = mRgba.cols();
-//        int rows = mRgba.rows();
-//
-//        int xOffset = (mOpenCvCameraView.getWidth() - cols) / 2;
-//        int yOffset = (mOpenCvCameraView.getHeight() - rows) / 2;
-//
-//        int x = (int)event.getX() - xOffset;
-//        int y = (int)event.getY() - yOffset;
-//
-//        Log.i(TAG, "Touch image coordinates: (" + x + ", " + y + ")");
-//
-//        if ((x < 0) || (y < 0) || (x > cols) || (y > rows)) return false;
-//
-//        Rect touchedRect = new Rect();
-//
-//        touchedRect.x = (x>4) ? x-4 : 0;
-//        touchedRect.y = (y>4) ? y-4 : 0;
-//
-//        touchedRect.width = (x+4 < cols) ? x + 4 - touchedRect.x : cols - touchedRect.x;
-//        touchedRect.height = (y+4 < rows) ? y + 4 - touchedRect.y : rows - touchedRect.y;
-//
-//        Mat touchedRegionRgba = mRgba.submat(touchedRect);
-//
-//        Mat touchedRegionHsv = new Mat();
-//        Imgproc.cvtColor(touchedRegionRgba, touchedRegionHsv, Imgproc.COLOR_RGB2HSV_FULL);
-//
-//        // Calculate average color of touched region
-//        mBlobColorHsv = Core.sumElems(touchedRegionHsv);
-//        int pointCount = touchedRect.width*touchedRect.height;
-//        for (int i = 0; i < mBlobColorHsv.val.length; i++)
-//            mBlobColorHsv.val[i] /= pointCount;
-//
-//        mBlobColorRgba = converScalarHsv2Rgba(mBlobColorHsv);
-//
-//        Log.i(TAG, "Touched rgba color: (" + mBlobColorRgba.val[0] + ", " + mBlobColorRgba.val[1] +
-//                ", " + mBlobColorRgba.val[2] + ", " + mBlobColorRgba.val[3] + ")");
-//
-//        mDetector.setHsvColor(mBlobColorHsv);
-//
-//        Imgproc.resize(mDetector.getSpectrum(), mSpectrum, SPECTRUM_SIZE, 0, 0, Imgproc.INTER_LINEAR_EXACT);
-//
-//        mIsColorSelected = true;
-//
-//        touchedRegionRgba.release();
-//        touchedRegionHsv.release();
-//
+        display_i = (display_i + 1) % segmenter.mDebugMats.size();
         return false; // don't need subsequent touch events
     }
 
     public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
-        mRgba = inputFrame.rgba();
-        segmenter.detection_candidates(inputFrame.gray());
-//
-//        if (mIsColorSelected) {
-//        mDetector.process(mRgba);
-//        List<MatOfPoint> contours = mDetector.getContours();
-//        Log.e(TAG, "Contours count: " + contours.size());
-//        Imgproc.drawContours(mRgba, contours, -1, CONTOUR_COLOR);
-//
-//        Mat colorLabel = mRgba.submat(4, 68, 4, 68);
-//        colorLabel.setTo(mBlobColorRgba);
-//
-//        Mat spectrumLabel = mRgba.submat(4, 4 + mSpectrum.rows(), 70, 70 + mSpectrum.cols());
-//        mSpectrum.copyTo(spectrumLabel);
-//    }
-//
-        return mRgba;
+        segmenter.detection_candidates(inputFrame.rgba());
+        return segmenter.mDebugMats.get(display_i);
     }
 }
--- ReadySetSet/app/src/main/java/com/example/readysetset/algorithm/Segmenter.java
+++ ReadySetSet/app/src/main/java/com/example/readysetset/algorithm/Segmenter.java
@@ -4,27 +4,103 @@ import android.util.Log;
 
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
+import java.util.ArrayList;
+
 public class Segmenter {
-    public Mat mScaled;
-    private int width;
-    private int height;
+    // stores the main operations
+    private Mat mMat;
+    private Mat mOrigSmall;
+    private Mat mResults;
+    private Mat mBinaryDisplay;
+    private Mat mBuffer;
+    
+    // the original mHeight and mWidth of the input frame
+    private int mWidth;
+    private int mHeight;
+    private ArrayList<MatOfPoint> mContours;
+    private MatOfPoint2f mHierarchy;
+    private ArrayList<MatOfPoint> mCandidates;
+    
+    // a list of useful debugging images
+    public ArrayList<Mat> mDebugMats;
+
+
+    private static Mat kernel = Mat.ones(3, 3, CvType.CV_32F);
 
     public Segmenter(int width, int height, double scale) {
-        this.width = (int)(width*scale);
-        this.height = (int)(height*scale);
+        this.mWidth = width;
+        this.mHeight = height;
 
-        mScaled = new Mat(width, height, CvType.CV_8UC1);
+        int width_small = (int)(width*scale);
+        int height_small = (int)(height*scale);
+
+        mMat = new Mat(width_small, height_small, CvType.CV_8UC1);
+        mBuffer = new Mat(width_small, height_small, CvType.CV_8UC4);
+        mOrigSmall = new Mat(width_small, height_small, CvType.CV_8UC4);
+        mResults = new Mat(width, height, CvType.CV_8UC4);
+        mBinaryDisplay = new Mat(width, height, CvType.CV_8UC4);
+        mContours = new ArrayList<>(30);
+        mCandidates = new ArrayList<>(30);
+        mHierarchy = new MatOfPoint2f();
+        mDebugMats = new ArrayList<>();
+        mDebugMats.add(mResults);
+        mDebugMats.add(mBinaryDisplay);
 
     }
 
     public void detection_candidates(Mat frame) {
-        Log.d("DEBUG", frame.size().toString());
-        Log.e("SET", frame.type() + "");
-        // TODO: this doesn't work
-        Imgproc.resize(frame, mScaled, new Size(width, height));
-        //Imgproc.GaussianBlur(mScaled, mScaled);
+        // make binary image
+        Imgproc.resize(frame, mOrigSmall, new Size(mWidth /3, mHeight /3));
+        Imgproc.cvtColor(mOrigSmall, mMat, Imgproc.COLOR_RGBA2GRAY);
+        Imgproc.GaussianBlur(mMat, mMat, new Size(5,5), 0);
+        Imgproc.threshold(mMat, mMat, 150, 255, Imgproc.THRESH_BINARY);
+        Imgproc.morphologyEx(mMat, mMat, Imgproc.MORPH_OPEN, kernel);
+
+        // save the binary image to mBinaryDisplay
+        Imgproc.cvtColor(mMat, mBuffer, Imgproc.COLOR_GRAY2RGBA);
+        Imgproc.resize(mBuffer, mBinaryDisplay, new Size(mWidth, mHeight));
+
+        // find contours
+        mContours.clear();
+        Imgproc.findContours(mMat, mContours, mHierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+
+        // filter contours
+        mCandidates.clear();
+        for (MatOfPoint contour : mContours) {
+            MatOfPoint2f buffer = new MatOfPoint2f(contour.toArray());
+            MatOfPoint2f outBuffer = new MatOfPoint2f();
+            Imgproc.approxPolyDP(buffer, outBuffer, 10, true);
+
+            if (outBuffer.toList().size() != 4) continue;
+            double area = Imgproc.contourArea(outBuffer);
+            if (area < 1000 || area > 20000) continue;
+
+            MatOfPoint mat = new MatOfPoint(outBuffer.toArray());
+            if(!mat.empty()) mCandidates.add(mat);
+        }
+
+        Imgproc.drawContours(mOrigSmall, mCandidates, -1, new Scalar(0,255,0), 3);
+        Imgproc.resize(mOrigSmall, mResults, new Size(mWidth, mHeight));
+    }
+
+    public void release() {
+        mMat.release();
+        mOrigSmall.release();
+        mResults.release();
+        mBinaryDisplay.release();
+        mBuffer.release();
+        for (MatOfPoint mat : mContours) {
+            mat.release();
+        }
+        for (MatOfPoint mat : mCandidates) {
+            mat.release();
+        }
+        mHierarchy.release();
     }
 }
