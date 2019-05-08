1f6129f9c9d9c15fe3daa2fa813a0c320fb711e2
Mitchell Roberts <mitchellroberts2015@gmail.com>
2019-05-08 17:27:39 -0400

    sped up card drawing by reducing resolution

--- cardDrawer.py
+++ cardDrawer.py
@@ -3,9 +3,9 @@ import numpy as np
 import sys
 
 colors = np.array([(90,152,44), (129,43,90), (56,68,189)])
-positions = [[[225, 375]],
-             [[225, 272], [225, 478]],
-             [[225, 167], [225, 375], [225, 583]]]
+positions = [[[225/450, 375/750]],
+             [[225/450, 272/750], [225/450, 478/750]],
+             [[225/450, 167/750], [225/450, 375/750], [225/450, 583/750]]]
 patterns = [cv2.imread('graphics/patterns/empty.png'),
             cv2.imread('graphics/patterns/solid.png'),
             cv2.imread('graphics/patterns/stripe.png')]
@@ -15,6 +15,15 @@ outlines = [cv2.imread('graphics/outlines/diamond.png'),
 centers = [cv2.imread('graphics/centers/diamond.png'),
             cv2.imread('graphics/centers/pill.png'),
             cv2.imread('graphics/centers/squiggle.png')]
+
+width = 75
+height = 45
+symbol_width = width * 165 // 750
+symbol_height = height * 315 // 450
+patterns = [cv2.resize(x, (symbol_width, symbol_height)) for x in patterns]
+outlines = [cv2.resize(x, (symbol_width, symbol_height)) for x in outlines]
+centers = [cv2.resize(x, (symbol_width, symbol_height)) for x in centers]
+positions = [[np.multiply([height, width], p).astype(np.int32) for p in n] for n in positions]
 
 def get_symbol(color, pattern, shape) :
     img = cv2.bitwise_or(outlines[shape], cv2.bitwise_and(centers[shape], patterns[pattern]))
@@ -27,7 +36,7 @@ def put_symbol(img, symbol, center) :
     img[tl[0]:tl[0]+symbol.shape[0], tl[1]:tl[1]+symbol.shape[1]] = symbol
 
 def draw_card(color, number, pattern, shape) :
-    img = np.zeros((450,750,3), np.uint8)
+    img = np.zeros((height,width,3), np.uint8)
     img[:,:] = (255,255,255)
     s = get_symbol(color,pattern,shape)
     for p in positions[number] :
@@ -37,9 +46,9 @@ def draw_card(color, number, pattern, shape) :
 def render_scene(cards, contours, shape) :
     img = np.zeros(shape, dtype=np.uint8)
     src = np.array([[0,0],
-           [750,0],
-           [750,450],
-           [0,450]], dtype=np.float32)
+           [width,0],
+           [width,height],
+           [0,height]], dtype=np.float32)
     for card, cont in zip(cards, contours) :
         M = cv2.getPerspectiveTransform(src, cont.astype(np.float32))
         raw_card = draw_card(card[0], card[1], card[2], card[3])
